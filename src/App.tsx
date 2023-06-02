f62931edabfd39fa198c11bceba21f4d70503dff
Elgar17 <1653794708@qq.com>
2023-06-02 23:48:18 +0800

    feat: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { ConfigProvider, Button } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+
+const config: ConfigProviderProps = {
+  direction: 'rtl',
+}
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <ConfigProvider direction="rtl">
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <div>
+            Vite + React
+            <Button>12</Button>
+            <FontAwesomeIcon icon={faHome} />
+          </div>
+        </div>
+      </ConfigProvider>
     </>
   )
 }
