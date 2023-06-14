7b187755cbc57b5750ab07bf9eff3f938c15e411
Elgar <45301805+Elgar17@users.noreply.github.com>
2023-06-14 09:15:01 +0800

    feat: add utils

---
+++ src/utils/file.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+
+export const readFile = (path: string) => {
+  return fs.readFile(path, "utf-8");
+};
+
+export const writeFile = (path: string, ctx: string) => {
+  return fs.writeFile(path, ctx, "utf-8");
+};
+
+export const renameFile = (path: string, newPath: string) => {
+  return fs.rename(path, newPath);
+};
+
+export const deleteFile = (path: string) => {
+  return fs.unlink(path);
+};
